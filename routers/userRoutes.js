91032b81d15e2b68021151b0fc51922f7f2cd9ad
Iulianna_Stepanova <iulianna_stepanova@epam.com>
2023-02-13 08:23:01 +0300

    err-user

--- routers/userRoutes.js
+++ routers/userRoutes.js
@@ -8,50 +8,58 @@ const userService = new UserService(model.User);
 
 const userSchema = require("../validations/user").userValidationSchema;
 
-router.get("/:id", (req, res) => {
+router.get("/:id", (req, res, next) => {
     const id = req.params.id;
-    userService.getUser(id).then(user => {
-        if (!user) {
-            return res.status(404).end();
-        } else {
-            res.send(user);
-        }
-    });
+    userService.getUser(id)
+        .then(user => {
+            if (!user) {
+                return res.status(404).end();
+            } else {
+                res.send(user);
+            }
+        })
+        .catch((err) => { next(err) });
 });
 
-router.post("/", validator.body(userSchema), (req, res) => {
+router.post("/", validator.body(userSchema), (req, res, next) => {
     const body = req.body;
-    userService.createUser(body).then(user => res.send(user));
+    userService.createUser(body).then(user => res.send(user)).catch((err) => { next(err) });;
 });
 
-router.get("/", (req, res) => {
-    userService.getAllUser().then(user => {
-        if (!user) {
-            return res.status(404).end();
-        } else {
-            res.send(user);
-        }
-    });
+router.get("/", (req, res, next) => {
+    userService.getAllUser()
+        .then(user => {
+            if (!user) {
+                return res.status(404).end();
+            } else {
+                res.send(user);
+            }
+        })
+        .catch((err) => { next(err) });
 });
 
-router.delete("/:id", (req, res) => {
+router.delete("/:id", (req, res, next) => {
     const id = req.params.id;
-    userService.deleteUser(id).then(user =>{ if (!user) {
-        return res.status(404).end();
-    }
-        res.send({ success: true });})
-
+    userService.deleteUser(id)
+        .then(user =>{ if (!user) {
+                return res.status(404).end();
+            }
+                res.send({ success: true });
+            })
+        .catch((err) => { next(err) });
 });
 
-router.put("/:id", validator.body(userSchema), (req, res) => {
+router.put("/:id", validator.body(userSchema), (req, res, next) => {
     const body = req.body;
     const id = req.params.id;
-    userService.updateUser(id, body).then(user => {
-        if (!user) {
-            return res.status(404).end();
-        }
-        res.send(user);
-    });
+    userService.updateUser(id, body)
+        .then(user => {
+            if (!user) {
+                return res.status(404).end();
+            }
+            res.send(user);
+        })
+        .catch((err) => { next(err) });
 });
 
 module.exports = router;
